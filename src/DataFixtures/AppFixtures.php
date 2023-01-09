2b6796122aa4982831b8b43629a2baadf7dd9496
Yazou777 <wilga.kevin@sfr.fr>
2023-01-09 16:00:28 +0100

    fixture

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Ingredient;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+      
+            $ingredient = new Ingredient();
+            $ingredient->setName('Ingredient #1')
+                ->setPrice(3.0);
+
+            $manager->persist($ingredient);
+
+            $manager->flush();
+    }
+}
