b893907eb29429a42eab13d450b688461ba7cc1e
Yazou777 <wilga.kevin@sfr.fr>
2023-01-13 10:34:09 +0100

    bootstraper recipe

--- src/Form/RecipeType.php
+++ src/Form/RecipeType.php
@@ -3,9 +3,15 @@
 namespace App\Form;
 
 use App\Entity\Recipe;
+use App\Entity\Ingredient;
+use App\Repository\IngredientRepository;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Validator\Constraints\Positive;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Form\Extension\Core\Type\RangeType;
 
 class RecipeType extends AbstractType
 {
@@ -15,13 +21,39 @@ class RecipeType extends AbstractType
             ->add('name')
             ->add('time')
             ->add('nbPeople')
-            ->add('difficulty')
+            ->add('difficulty', RangeType::class, [
+                'attr' => [
+                    'class' => 'form-range',
+                    'min' => 1,
+                    'max' => 5
+                ],
+                'required' => false,
+                'label' => 'Difficulté',
+                'label_attr' => [
+                    'class' => 'form-label mt-4'
+                ],
+                'constraints' => [
+                    new Positive(),
+                    new Assert\LessThan(5)
+                ]
+            ])
+
             ->add('description')
             ->add('price')
             ->add('isFavorite')
             ->add('createdAt')
             ->add('updateAt')
-            ->add('ingredients')
+            ->add('ingredients', EntityType::class, [
+                'class' => Ingredient::class,
+                'query_builder' => function (IngredientRepository $r) {
+                    return $r->createQueryBuilder('i')
+                        ->orderBy('i.name', 'ASC');
+                },
+                'choice_label' => 'name',
+                'multiple' => true,
+                'expanded' => true,
+            ])
+
         ;
     }
 
